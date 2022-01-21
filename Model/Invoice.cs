1e3fb335da109a79c720f8655636c6b3d1de379a
jonaswahringer <jonas.wahringer@gmail.com>
2022-01-21 14:55:15 +0100

    first commit

---
+++ Model/Invoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyERP.Model
+{
+    class Invoice
+    {
+        int Id { get; set; }
+        string CustomerName { get; set; }
+        string CustomerAddress { get; set; }
+        double Amount { get; set; }
+        DateTime InvoiceDate { get; set; }
+        int Vat { get; set; }
+    }
+}
