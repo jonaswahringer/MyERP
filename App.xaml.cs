850799a988c60312d7dd1c47ce48e06ce5a2b16e
raphi <raphi.kapeller@gmx.at>
2022-01-27 09:21:25 +0100

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _4_06_EF_ERP
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
