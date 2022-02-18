98246a3b526bc50eb90318038584edf94ad2cf9b
r4phi_laptop <raphael.kapeller@sz-ybbs.ac.at>
2022-02-18 11:19:06 +0100

    Initializer

--- Context/InvoiceInitializer.cs
+++ Context/InvoiceInitializer.cs
@@ -21,6 +21,20 @@ namespace _4_06_EF_ERP.Context
                 Qty = 10
             });
 
+            defaultPositions.Add(new Position
+            {
+                ItemNr = 0,
+                Price = 100,
+                Qty = 10
+            });
+
+            defaultPositions.Add(new Position
+            {
+                ItemNr = 0,
+                Price = 100,
+                Qty = 10
+            });
+
             defaults.Add(new Invoice
             {
                 CustomerName = "HTL",
@@ -28,7 +42,7 @@ namespace _4_06_EF_ERP.Context
                 CustomerAddress = "Ybbs",
                 InvoiceDate = new DateTime(2020, 01, 25),
                 Vat = 20,
-                Positions = defaultPositions
+                //Positions = defaultPositions
             });
 
             defaults.Add(new Invoice
@@ -38,7 +52,35 @@ namespace _4_06_EF_ERP.Context
                 CustomerAddress = "Ybbs",
                 InvoiceDate = new DateTime(2020, 01, 15),
                 Vat = 20,
-                Positions = defaultPositions
+                //Positions = defaultPositions
+            });
+
+            defaults.Add(new Invoice
+            {
+                CustomerName = "HAS",
+                Amount = 300,
+                CustomerAddress = "Ybbs",
+                InvoiceDate = new DateTime(2020, 01, 10),
+                Vat = 20,
+                //Positions = defaultPositions
+            });
+
+            defaults.Add(new Invoice
+            {
+                CustomerName = "Mittelschule",
+                Amount = 400,
+                CustomerAddress = "Ybbs",
+                InvoiceDate = new DateTime(2020, 01, 05),
+                Vat = 20
+            });
+
+            defaults.Add(new Invoice
+            {
+                CustomerName = "Volksschule",
+                Amount = 500,
+                CustomerAddress = "Ybbs",
+                InvoiceDate = new DateTime(2020, 01, 01),
+                Vat = 20
             });
 
             context.Invoices.AddRange(defaults);
